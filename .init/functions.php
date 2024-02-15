1da242e922a03fdc91cc6436929efd598d150f05
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-15 22:00:20 +0900

    update: Makefile整備

--- .init/functions.php
+++ .init/functions.php
@@ -10,10 +10,7 @@ $wpmvc_app = new System\Application\Application();
  * Configure application(comment out & edit default value for required)
  * --------------------------------------------------------------------
  */
-// $wpmvc_app->addConfig(CONFIG::DEBUG, 'false');
-// $wpmvc_app->addConfig(CONFIG::BOOTSTRAP, __WPMVC_ROOT__ . '/src/App/bootstrap.php');
-// $wpmvc_app->addConfig(CONFIG::CONTROLLER_NAMESPACE, 'App\Controllers');
-// $wpmvc_app->addConfig(CONFIG::VIEW_DIRECTORY, __WPMVC_ROOT__ . '/src/App/Views/');
+
 
 /*
  * --------------------------------------------------------------------
