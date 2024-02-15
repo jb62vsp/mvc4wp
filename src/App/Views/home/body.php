8450dfa4f04bc542409d7799b3085ef0af5ce0f8
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-15 13:15:29 +0900

    fix: サンプルのView配置

--- src/App/Views/home/body.php
+++ src/App/Views/home/body.php
@@ -1,6 +1,6 @@
 <h1>
     <?php echo "Hello {$data['test']}"; ?>
 </h1>
-<?php $data['this']->view('parts/register'); ?>
-<?php $data['this']->view('parts/update'); ?>
-<?php $data['this']->view('parts/delete'); ?>
+<?php $data['this']->view('home/parts/register'); ?>
+<?php $data['this']->view('home/parts/update'); ?>
+<?php $data['this']->view('home/parts/delete'); ?>
