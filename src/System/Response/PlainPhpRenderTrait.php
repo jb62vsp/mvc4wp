263f084e58ed53b7e5161492ce73cf18924140be
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-15 10:07:23 +0900

    fix: '/'の置き換え

--- src/System/Response/PlainPhpRenderTrait.php
+++ src/System/Response/PlainPhpRenderTrait.php
@@ -10,7 +10,7 @@ trait PlainPhpRenderTrait
     public function render(ConfigInterface $config, ResponderInterface $responder, string $view, array $data = []): self
     {
         $responder->header('Content-Type: text/html; charset=utf-8');
-        $view_path = $config->getConfig(CONFIG::VIEW_DIRECTORY) . '/' . $view . '.php';
+        $view_path = $config->getConfig(CONFIG::VIEW_DIRECTORY) . DIRECTORY_SEPARATOR . $view . '.php';
         if (file_exists($view_path)) {
             include_once $view_path;
         }
