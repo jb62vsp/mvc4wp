f9292f0a9a0155731d011f0812492777efd58c53
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-16 18:21:16 +0900

    コメントアウト

--- functions.php
+++ functions.php
@@ -10,9 +10,9 @@ $wpmvc_app = new System\Application\Application();
  * Configure application(comment out & edit default value for required)
  * --------------------------------------------------------------------
  */
-$wpmvc_app->config()->add(System\Config\CONFIG::DEBUG, 'false');
-$wpmvc_app->config()->add(System\Config\CONFIG::CONTROLLER_NAMESPACE, 'App\Controllers');
-$wpmvc_app->config()->add(System\Config\CONFIG::VIEW_DIRECTORY, __WPMVC_ROOT__ . '/src/App/Views/');
+// $wpmvc_app->config()->add(System\Config\CONFIG::DEBUG, 'false');
+// $wpmvc_app->config()->add(System\Config\CONFIG::CONTROLLER_NAMESPACE, 'App\Controllers');
+// $wpmvc_app->config()->add(System\Config\CONFIG::VIEW_DIRECTORY, __WPMVC_ROOT__ . '/src/App/Views/');
 
 /*
  * --------------------------------------------------------------------
