04e891ec1bf807443c3df06f009ccd6e35799ed2
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-17 23:41:56 +0900

    refactor: #14

--- src/System/Application/Application.php
+++ src/System/Application/Application.php
@@ -3,27 +3,25 @@ namespace System\Application;
 
 use System\Config\ConfigInterface;
 use System\Config\DefaultConfigurator;
-use System\Controllers\DefaultHttpErrorController;
 use System\Core\Cast;
-use System\Core\HttpStatus;
-use System\Exception\ApplicationException;
 use System\Route\DefaultRouter;
-use System\Route\RouteHandler;
 use System\Route\RouterInterface;
-use System\Service\Logging;
 
 final class Application implements ApplicationInterface
 {
-    use Cast;
+    use Cast, ApplicationTrait;
 
     public function __construct(
-        private ConfigInterface $config = new DefaultConfigurator(),
-        private RouterInterface $router = new DefaultRouter(),
+        ConfigInterface $config = new DefaultConfigurator(),
+        RouterInterface $router = new DefaultRouter(),
     ) {
+        $this->config = $config;
+        $this->router = $router;
+
         /*
          * -------- DEFAULT CONFIGURATIONS --------
          */
-        $this->config()->add(\System\Config\CONFIG::DEBUG, 'false');
+        $this->config()->add(\System\Config\CONFIG::DEBUG, 'false'); // TODO:
         $this->config()->add(\System\Config\CONFIG::LOGGER, [
             'default_logger_name' => 'app',
             'loggers' => [
@@ -45,57 +43,11 @@ final class Application implements ApplicationInterface
         ]);
         $this->config()->add(\System\Config\CONFIG::CONTROLLER_NAMESPACE, 'App\Controllers');
         $this->config()->add(\System\Config\CONFIG::VIEW_DIRECTORY, __WPMVC_ROOT__ . '/src/App/Views/');
-    }
-
-    public function config(): ConfigInterface
-    {
-        return $this->config;
-    }
-
-    public function router(): RouterInterface
-    {
-        return $this->router;
+        include_once(__WPMVC_ROOT__ . '/src/System/Core/Common.php');
     }
 
     public function run(): void
     {
-        include_once(__WPMVC_ROOT__ . '/src/System/Core/Common.php');
-        $this->execute($this->config(), $this->router());
-    }
-
-    public function execute(ConfigInterface $config, RouterInterface $router): void
-    {
-        $request_method = strtoupper($_SERVER['REQUEST_METHOD']);
-        if (isset($_POST['_method'])) {
-            $request_method = strtoupper($_POST['_method']);
-        } elseif (isset($_POST['_METHOD'])) {
-            $request_method = strtoupper($_POST['_METHOD']);
-        }
-
-        /** @var RouteHandler $route */
-        $route = $router->dispatch($config, $request_method, $_SERVER['REQUEST_URI']);
-
-        if ($route->status !== HttpStatus::OK) {
-            $controller = new DefaultHttpErrorController($config, $route->status);
-            $controller->index();
-            return;
-        }
-
-        if (!class_exists($route->class)) {
-            throw new ApplicationException();
-        }
-
-        $controller = new $route->class($this->config());
-        if (!method_exists($controller, $route->method)) {
-            throw new ApplicationException();
-        }
-
-        if (method_exists($controller, 'init')) {
-            Logging::get('system')->debug($_SERVER['REQUEST_URI'] . ' => ' . $route->class . '->init', $route->args);
-            $controller->init($route->args);
-        }
-
-        Logging::get('system')->debug($_SERVER['REQUEST_URI'] . ' => ' . $route->class . '->' . $route->method, $route->args);
-        $controller->{$route->method}($route->args);
+        $this->executeController($this->config(), $this->router());
     }
 }
