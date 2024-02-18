158c7b3fa1e74f625c288fedf2a45eb68ab9c2a6
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-18 10:55:23 +0900

    refactor: #23 不要なuse traitを削除

--- src/System/Route/Default/DefaultRouter.php
+++ src/System/Route/Default/DefaultRouter.php
@@ -3,9 +3,8 @@ namespace System\Route\Default;
 
 use System\Core\Cast;
 use System\Route\AbstractRouter;
-use System\Route\RouterTrait;
 
 class DefaultRouter extends AbstractRouter
 {
-    use Cast, FastRouteRouterTrait, RouterTrait;
+    use Cast, FastRouteRouterTrait;
 }
