88c615c3a718e661c741beef68ac455aa91df1b3
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-19 13:07:28 +0900

    style: reduce indent

--- src/System/Application/Default/DefaultApplication.php
+++ src/System/Application/Default/DefaultApplication.php
@@ -24,21 +24,21 @@ class DefaultApplication extends AbstractApplication
         $this->config()->add(\System\Config\CONFIG::LOGGER, [
             'default_logger_name' => 'app',
             'loggers' => [
-                    'app' => [
-                        'logger_factory' => '\System\Logger\FileLoggerFactory',
-                        'directory' => __WPMVC_ROOT__ . '/log/',
-                        'basefilename' => 'app',
-                        'date_format' => 'Ymd',
-                        'log_level' => 'debug',
-                    ],
-                    'system' => [
-                        'logger_factory' => '\System\Logger\FileLoggerFactory',
-                        'directory' => __WPMVC_ROOT__ . '/log/',
-                        'basefilename' => 'sys',
-                        'date_format' => 'Ymd',
-                        'log_level' => 'debug',
-                    ],
+                'app' => [
+                    'logger_factory' => '\System\Logger\FileLoggerFactory',
+                    'directory' => __WPMVC_ROOT__ . '/log/',
+                    'basefilename' => 'app',
+                    'date_format' => 'Ymd',
+                    'log_level' => 'debug',
                 ],
+                'system' => [
+                    'logger_factory' => '\System\Logger\FileLoggerFactory',
+                    'directory' => __WPMVC_ROOT__ . '/log/',
+                    'basefilename' => 'sys',
+                    'date_format' => 'Ymd',
+                    'log_level' => 'debug',
+                ],
+            ],
         ]);
         $this->config()->add(\System\Config\CONFIG::CONTROLLER_NAMESPACE, 'App\Controllers');
         $this->config()->add(\System\Config\CONFIG::VIEW_DIRECTORY, __WPMVC_ROOT__ . '/src/App/Views/');
