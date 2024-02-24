4d5e2aebf9e94f32e2a6afe53f743f7bc5039449
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-24 15:12:12 +0900

    fix: マージ後のエラー箇所を修正

--- functions.php
+++ functions.php
@@ -4,12 +4,15 @@ use App\Model\ExampleEntity;
 use App\Model\LogEntity;
 use Mvc4Wp\Core\Library\WordPressCustomize;
 use Mvc4Wp\Core\Service\App;
+use Mvc4Wp\Core\Service\Logging;
 
 define('__MVC4WP_ROOT__', __DIR__);
 require_once(__MVC4WP_ROOT__ . '/vendor/autoload.php');
 
-App::get()->config()->set('LOGGER', 'debug', 'loggers', 'app', 'log_level');
-App::get()->config()->set('LOGGER', 'debug', 'loggers', 'core', 'log_level');
+App::get()->config()->set('logger.loggers.app.log_level', 'debug');
+App::get()->config()->set('logger.loggers.core.log_level', 'debug');
+
+Logging::configure(App::get()->config());
 
 WordPressCustomize::addCustomPostType(ExampleEntity::class);
 WordPressCustomize::addCustomPostType(LogEntity::class);
