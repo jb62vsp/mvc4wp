e99ef1e78daa8dc5bf34333b69cd11bb5daf1583
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-25 17:38:31 +0900

    feat: マージ後の修正

--- src/App/Controller/AdminController.php
+++ src/App/Controller/AdminController.php
@@ -3,15 +3,19 @@ namespace App\Controller;
 
 use Mvc4Wp\Core\Controller\PlainPhpController;
 use Mvc4Wp\Core\Library\Castable;
+use Mvc4Wp\Core\Model\UserEntity;
 use Mvc4Wp\Core\Service\Logging;
 
 abstract class AdminController extends PlainPhpController
 {
     use Castable;
 
+    protected UserEntity $login_user;
+
     public function init(array $args = []): void
     {
-        if (!is_user_logged_in()) {
+        $this->login_user = UserEntity::current();
+        if (is_null($this->login_user)) {
             Logging::get()->notice('Not logged in user access to ' . $_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['REQUEST_URI'] . ' => ' . static::class . ', from: ' . $_SERVER['REMOTE_ADDR']);
             $this->notFound()->done();
         }
