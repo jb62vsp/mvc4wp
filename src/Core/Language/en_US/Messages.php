291bf05862bc3f718233842a2b8705ef66e4c0b1
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-26 17:19:27 +0900

    feat: #50 RegExpRuleを修正しテストを作成

--- src/Core/Language/en_US/Messages.php
+++ src/Core/Language/en_US/Messages.php
@@ -5,5 +5,6 @@ $messages = [
         'MinLengthRule' => 'The {field} must be at least {minimum} characters.',
         'MaxLengthRule' => 'The {field} must be fall short of {max} characters.',
         'LengthRule' => 'The {field} must be within {minimum} to {max} characters.',
+        'RegExpRule' => 'The {field} is not in the correct format.',
     ],
 ];
--- src/Core/Language/ja/Messages.php
+++ src/Core/Language/ja/Messages.php
@@ -5,5 +5,6 @@ $messages = [
         'MinLengthRule' => '{field}は、{minimum}文字以上で入力してください。',
         'MaxLengthRule' => '{field}は、{max}文字以内で入力してください。',
         'LengthRule' => '{field}は、{minimum}文字以上、{max}文字以内で入力してください。',
+        'RegExpRule' => '{field}が、正しい形式ではありません。'
     ],
 ];
