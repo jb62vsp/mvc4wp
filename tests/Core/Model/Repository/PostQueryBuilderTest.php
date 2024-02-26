7bfd94bd660754e59e6de6c4ccb81250e05af068
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-02-26 09:35:26 +0900

    test: コメントアウトしていたのをとりあえず復帰

--- tests/Core/Model/Repository/PostQueryBuilderTest.php
+++ tests/Core/Model/Repository/PostQueryBuilderTest.php
@@ -1,10 +1,16 @@
 <?php declare(strict_types=1);
-// namespace Mvc4Wp\Core\Model\Repository;
+namespace Mvc4Wp\Core\Model\Repository;
 
-// use PHPUnit\Framework\Attributes\CoversClass;
-// use PHPUnit\Framework\TestCase;
+use Mvc4Wp\Core\Model\PostEntity;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 
-// #[CoversClass(PostQueryBuilder::class)]
-// class PostQueryBuilderTest extends TestCase
-// {
-// }
+#[CoversClass(PostQueryBuilder::class)]
+class PostQueryBuilderTest extends TestCase
+{
+    public function test_new(): void
+    {
+        $obj = new PostQueryBuilder(PostEntity::class);
+        $this->assertNotNull($obj);
+    }
+}
