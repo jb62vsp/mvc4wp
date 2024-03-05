456f3a4fc17b3c363783e079ff6018c39d290e81
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-03-06 07:50:59 +0900

    fix: 関数大文字化

--- index.php
+++ index.php
@@ -11,35 +11,35 @@ use Mvc4Wp\Core\Service\Helper;
  * --------------------------------------------------------------------
  */
 // controller sample
-App::get()->router()->get('/', 'HomeController::index');
-App::get()->router()->get('/index|/home|/home/|/home/index', 'HomeController::index');
-App::get()->router()->get('/home/other/{id:\d+}', 'HomeController::other');
-App::get()->router()->get('/home/redirect', 'HomeController::redirect');
+App::get()->router()->GET('/', 'HomeController::index');
+App::get()->router()->GET('/index|/home|/home/|/home/index', 'HomeController::index');
+App::get()->router()->GET('/home/other/{id:\d+}', 'HomeController::other');
+App::get()->router()->GET('/home/redirect', 'HomeController::redirect');
 // json response sample
-App::get()->router()->get('/api|/api/', 'ApiController::index');
-App::get()->router()->post('/api|/api/', 'ApiController::post');
+App::get()->router()->GET('/api|/api/', 'ApiController::index');
+App::get()->router()->POST('/api|/api/', 'ApiController::post');
 // post sample
-App::get()->router()->get('/post|/post/|/post/index', 'PostController::index');
-App::get()->router()->get('/post/list', 'PostController::list');
-App::get()->router()->get('/post/list/{sort}', 'PostController::list');
-App::get()->router()->get('/post/list/{sort}/{order}', 'PostController::list');
-App::get()->router()->get('/post/{id:\d+}', 'PostController::single');
-App::get()->router()->post('/post/', 'PostController::register');
-App::get()->router()->put('/post/{id:\d+}', 'PostController::update');
-App::get()->router()->delete('/post/{id:\d+}', 'PostController::delete');
+App::get()->router()->GET('/post|/post/|/post/index', 'PostController::index');
+App::get()->router()->GET('/post/list', 'PostController::list');
+App::get()->router()->GET('/post/list/{sort}', 'PostController::list');
+App::get()->router()->GET('/post/list/{sort}/{order}', 'PostController::list');
+App::get()->router()->GET('/post/{id:\d+}', 'PostController::single');
+App::get()->router()->POST('/post/', 'PostController::register');
+App::get()->router()->PUT('/post/{id:\d+}', 'PostController::update');
+App::get()->router()->DELETE('/post/{id:\d+}', 'PostController::delete');
 // custom example
-App::get()->router()->get('/example|/example/|/example/index', 'ExampleController::index');
-App::get()->router()->get('/example/list', 'ExampleController::list');
-App::get()->router()->get('/example/list/{sort}', 'ExampleController::list');
-App::get()->router()->get('/example/list/{sort}/{order}', 'ExampleController::list');
-App::get()->router()->get('/example/list/{sort}/{order}/{page:\d+}', 'ExampleController::list');
-App::get()->router()->get('/example/list/{sort}/{order}/{page:\d+}/{per_page:\d+}', 'ExampleController::list');
-App::get()->router()->get('/example/search', 'ExampleController::list');
-App::get()->router()->post('/example/search', 'ExampleController::search');
-App::get()->router()->get('/example/{id:\d+}', 'ExampleController::single');
-App::get()->router()->post('/example/', 'ExampleController::register');
-App::get()->router()->put('/example/{id:\d+}', 'ExampleController::update');
-App::get()->router()->delete('/example/{id:\d+}', 'ExampleController::delete');
+App::get()->router()->GET('/example|/example/|/example/index', 'ExampleController::index');
+App::get()->router()->GET('/example/list', 'ExampleController::list');
+App::get()->router()->GET('/example/list/{sort}', 'ExampleController::list');
+App::get()->router()->GET('/example/list/{sort}/{order}', 'ExampleController::list');
+App::get()->router()->GET('/example/list/{sort}/{order}/{page:\d+}', 'ExampleController::list');
+App::get()->router()->GET('/example/list/{sort}/{order}/{page:\d+}/{per_page:\d+}', 'ExampleController::list');
+App::get()->router()->GET('/example/search', 'ExampleController::list');
+App::get()->router()->POST('/example/search', 'ExampleController::search');
+App::get()->router()->GET('/example/{id:\d+}', 'ExampleController::single');
+App::get()->router()->POST('/example/', 'ExampleController::register');
+App::get()->router()->PUT('/example/{id:\d+}', 'ExampleController::update');
+App::get()->router()->DELETE('/example/{id:\d+}', 'ExampleController::delete');
 
 /*
  * --------------------------------------------------------------------
