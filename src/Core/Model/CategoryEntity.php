5b39b5ba3ce4458b6e9b6d2623f59296a37c82e4
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-03-06 21:42:42 +0900

    feat: Termの登録更新削除を実装

--- src/Core/Model/CategoryEntity.php
+++ src/Core/Model/CategoryEntity.php
@@ -2,6 +2,7 @@
 namespace Mvc4Wp\Core\Model;
 
 use Mvc4Wp\Core\Library\Castable;
+use Mvc4Wp\Core\Model\Attribute\CustomField;
 use Mvc4Wp\Core\Model\Attribute\Entry;
 use Mvc4Wp\Core\Model\Attribute\Field;
 use Mvc4Wp\Core\Model\Repository\TermQueryBuilder;
@@ -10,6 +11,9 @@ use Mvc4Wp\Core\Model\Repository\TermQueryBuilder;
 class CategoryEntity extends TermEntity
 {
     use Castable;
+
+    #[Field]
+    public int $term_group;
 
     #[Field]
     public int $parent;
@@ -35,16 +39,39 @@ class CategoryEntity extends TermEntity
 
     public function register(bool $publish = true): int
     {
+        $term_ids = wp_insert_term($this->name, $this->taxonomy, [
+            'description' => $this->description,
+            'parent' => $this->parent,
+            'slug' => $this->slug,
+        ]);
+        $this->bind($term_ids);
+        $properties = CustomField::getAttributedProperties(get_class($this));
+        foreach ($properties as $property) {
+            $untypedValue = static::toString($this, $property);
+            $property = $property->getName();
+            update_term_meta($this->term_id, $property, $untypedValue);
+        }
         return $this->term_id;
     }
 
     public function update(): void
     {
-
+        wp_update_term($this->term_id, $this->taxonomy, [
+            'description' => $this->description,
+            'parent' => $this->parent,
+            'slug' => $this->slug,
+        ]);
+        $properties = CustomField::getAttributedProperties(get_class($this));
+        foreach ($properties as $property) {
+            $untypedValue = static::toString($this, $property);
+            $property = $property->getName();
+            update_term_meta($this->term_id, $property, $untypedValue);
+        }
     }
 
     public function delete(bool $force_delete = false): bool
     {
-        return true;
+        $result = wp_delete_term($this->term_id, $this->taxonomy);
+        return $result;
     }
 }
