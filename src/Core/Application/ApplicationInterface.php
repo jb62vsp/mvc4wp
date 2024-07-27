c988a77b11aed45bb445d4fa31a04b6ce9b8c0e4
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-07-27 23:11:43 +0900

    feat: エラーレスポンスを返す際、エラーハンドラからコントローラを参照する

--- src/Core/Application/ApplicationInterface.php
+++ src/Core/Application/ApplicationInterface.php
@@ -5,6 +5,7 @@ use Mvc4Wp\Core\Config\ConfiguratorInterface;
 use Mvc4Wp\Core\Controller\ControllerInterface;
 use Mvc4Wp\Core\Language\MessagerInterface;
 use Mvc4Wp\Core\Library\ClockInterface;
+use Mvc4Wp\Core\Library\HttpStatus;
 use Mvc4Wp\Core\Route\RouterInterface;
 
 interface ApplicationInterface
@@ -14,6 +15,8 @@ interface ApplicationInterface
     public function config(): ConfiguratorInterface;
 
     public function controller(): ControllerInterface;
+
+    public function errorHandler(HttpStatus $httpStatus): ControllerInterface;
 
     public function messager(): MessagerInterface;
 
