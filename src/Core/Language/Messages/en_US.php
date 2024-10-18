6b164b8c5b396684efedfe8952adce65f084860c
Satoshi Hino <surroundedbyfoes@gmail.com>
2024-10-18 16:10:41 +0900

    feat: #60 カスタマイズの多言語対応

--- src/Core/Language/Messages/en_US.php
+++ src/Core/Language/Messages/en_US.php
@@ -1,6 +1,14 @@
 <?php declare(strict_types=1);
 
 $messages = [
+    'wordpress_customize' => [
+        'message' => [
+            'integer' => '* Only integer.',
+            'unsigned_integer' => '* Only unsigned integer.',
+            'float' => '* Only float.',
+            'unsigned_float' => '* Only unsigned float',
+        ],
+    ],
     'validation' => [
         'RequiredRule' => 'The {field} is required.',
         'MinLengthRule' => 'The {field} must be at least {minimum} characters.',
--- src/Core/Language/Messages/ja.php
+++ src/Core/Language/Messages/ja.php
@@ -1,6 +1,14 @@
 <?php declare(strict_types=1);
 
 $messages = [
+    'wordpress_customize' => [
+        'message' => [
+            'integer' => '※正負の整数のみ入力できます。',
+            'unsigned_integer' => '※正の整数のみ入力できます。',
+            'float' => '※正負の整数、小数のみ入力できます。',
+            'unsigned_float' => '※正の整数、小数のみ入力できます。',
+        ],
+    ],
     'validation' => [
         'RequiredRule' => '{field}は、必須です。',
         'MinLengthRule' => '{field}は、{minimum}文字以上で入力してください。',
