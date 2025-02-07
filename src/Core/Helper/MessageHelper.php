824e1ea97189aa154d7b036ebf23ba2b4b58508a
Satoshi Hino <surroundedbyfoes@gmail.com>
2025-02-07 15:24:33 +0900

    feat: DateTimeUtilsに関数を追加 fix: MessageHelperが使いづらいので修正

--- src/Core/Helper/MessageHelper.php
+++ src/Core/Helper/MessageHelper.php
@@ -2,16 +2,9 @@
 
 use Mvc4Wp\Core\Service\App;
 
-if (!function_exists('message_key')) {
-    function message_key(string $message_key, array $args = []): string
+if (!function_exists('msg')) {
+    function msg(string $message_key, array $args = []): string
     {
         return App::get()->messager()->message($message_key, $args);
-    }
-}
-
-if (!function_exists('message')) {
-    function message(string $message, array $args = []): string
-    {
-        return App::get()->messager()->message('', $args, $message);
     }
 }
