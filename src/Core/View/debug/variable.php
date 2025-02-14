f2f8174ead62e0304dcd5cff30e4b5e26529e136
Satoshi Hino <surroundedbyfoes@gmail.com>
2025-02-14 19:32:41 +0900

    fix: デバッグコンソールの変数を見やすく修正

--- src/Core/View/debug/variable.php
+++ src/Core/View/debug/variable.php
@@ -24,20 +24,13 @@ $g = [
             <?php if (array_key_exists('var', $mvc4wp_debug) && !empty($mvc4wp_debug['var'])): ?>
                 <?php foreach ($mvc4wp_debug['var'] as $var): ?>
                     <?php foreach ($var as $k => $v): ?>
-                        <p>
-                            <span class='name debug-cyan'>
-                                <?php eh($k); ?>
-                            </span>
-                            <?php if (is_array($v) || is_object($v)): ?>
-                                <span class='value'>
-                                    <pre class='debug-green'><?php eh(print_r($v, true)); ?></pre>
-                                </span>
-                            <?php else: ?>
-                                <span class='debug-green'>
-                                    <?php eh(print_r($v, true)); ?>
-                                </span>
-                            <?php endif; ?>
-                        </p>
+                        <input type='checkbox' id='debug-variable-named-<?php eh($k); ?>-toggle' class='checkbox'>
+                        <label for='debug-variable-named-<?php eh($k); ?>-toggle' class='label debug-clickable'>
+                            <h4><i class="icon-plus"></i><?php eh($k); ?></h4>
+                        </label>
+                        <div class='expandable'>
+                            <pre class='debug-green'><?php eh(print_r($v, true)); ?></pre>
+                        </div>
                     <?php endforeach; ?>
                 <?php endforeach; ?>
             <?php endif; ?>
