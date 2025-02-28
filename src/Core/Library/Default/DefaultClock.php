6eed79ebd376fcbf7daee1397aa84bbd6d23eecc
Satoshi Hino <surroundedbyfoes@gmail.com>
2025-02-28 19:05:22 +0900

    fix: DateTime直接使用を廃止

--- src/Core/Library/Default/DefaultClock.php
+++ src/Core/Library/Default/DefaultClock.php
@@ -1,17 +1,24 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace Mvc4Wp\Core\Library\Default;
 
-use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
 use DateTimeZone;
+use Mvc4Wp\Core\Library\Castable;
 use Mvc4Wp\Core\Library\ClockInterface;
 
 class DefaultClock implements ClockInterface
 {
-    public static function get(string $datetime = "now", DateTimeZone $timezone = null): DateTime
+    use Castable;
+
+    public static function get(string $datetime = "now", DateTimeZone $timezone = null): DateTimeInterface
     {
         if (is_null($timezone)) {
             $timezone = new DateTimeZone(ini_get('date.timezone'));
         }
-        return new DateTime($datetime, $timezone);
+        return new DateTimeImmutable($datetime, $timezone);
     }
 }
